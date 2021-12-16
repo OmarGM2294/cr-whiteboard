9ca48bf0f6aea506cd88f6a1fabc607140dd5fae
Omar Gonzalez <ogonzalez2212@gmail.com>
2021-12-16 06:26:02 -0500

    Home icon
    Changes:
    - Home icon added to go back to whiteboard view

--- components/Nav.js
+++ components/Nav.js
@@ -1,17 +1,25 @@
+import Link from 'next/link'
+
 import { useSession } from "next-auth/react"
 import { signOut } from "next-auth/react"
+import { FaHome } from 'react-icons/fa'
 
 const Nav = () => {
   const { data: session } = useSession()
 
   return (
-    <div className="flex flex-row gap-4 absolute right-5 top-5">
+    <div className="flex flex-row gap-4 absolute right-5 top-5 items-center">
       { session ?
         <img
           src={session.user.image}
           className="w-10 h-10 rounded-full shadow-lg border-2 border-white"
         />
       : null }
+      <Link
+        href="/whiteboards"
+      >
+        <FaHome className="text-4xl cursor-pointer" />
+      </Link>
       <button
         onClick={signOut}
         className="border-2 border-slate-700 rounded-lg px-3 py-1"
